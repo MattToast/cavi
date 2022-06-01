41faa1085f8ef0b796938c7b002e81ae082a2cc0
MattToast <matthew.drozt@gmail.com>
2022-05-31 21:14:25 -0500

    add tests for status getters

---
+++ tests/test_status_getters.py
@@ -0,0 +1,21 @@
+from cavi import status
+
+
+def test_get_abort():
+    status.get_abort()
+
+
+def test_get_launch():
+    status.get_launch()
+
+
+def test_get_qdm():
+    status.get_qdm()
+
+
+def test_get_stabilize():
+    status.get_stabilize()
+
+
+def test_get_arm():
+    status.get_arm()
