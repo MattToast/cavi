81ba559850ce2d2118470bda5b4ccb075a2f9a8f
MattToast <matthew.drozt@gmail.com>
2022-06-12 00:49:51 -0500

    use test build macro for each data getter

--- src/_cavi/src/py_getdata.cc
+++ src/_cavi/src/py_getdata.cc
@@ -19,55 +19,100 @@ getAltitude(PyObject* self)
 PyObject*
 getLatitude(PyObject* self)
 {
+#ifdef CAVI_TEST_BUILD
   return PyFloat_FromDouble(99.99L);
+#else
+  // TODO: Add something to read from sensor data
+  return PyFloat_FromDouble(0.0L);
+#endif
 }
 
 PyObject*
 getLongitude(PyObject* self)
 {
+#ifdef CAVI_TEST_BUILD
   return PyFloat_FromDouble(99.99L);
+#else
+  // TODO: Add something to read from sensor data
+  return PyFloat_FromDouble(0.0L);
+#endif
 }
 
 PyObject*
 getTemperature(PyObject* self)
 {
+#ifdef CAVI_TEST_BUILD
   return PyFloat_FromDouble(99.99L);
+#else
+  // TODO: Add something to read from sensor data
+  return PyFloat_FromDouble(0.0L);
+#endif
 }
 
 PyObject*
 getGyroX(PyObject* self)
 {
+#ifdef CAVI_TEST_BUILD
   return PyFloat_FromDouble(99.99L);
+#else
+  // TODO: Add something to read from sensor data
+  return PyFloat_FromDouble(0.0L);
+#endif
 }
 
 PyObject*
 getGyroY(PyObject* self)
 {
+#ifdef CAVI_TEST_BUILD
   return PyFloat_FromDouble(99.99L);
+#else
+  // TODO: Add something to read from sensor data
+  return PyFloat_FromDouble(0.0L);
+#endif
 }
 
 PyObject*
 getGyroZ(PyObject* self)
 {
+#ifdef CAVI_TEST_BUILD
   return PyFloat_FromDouble(99.99L);
+#else
+  // TODO: Add something to read from sensor data
+  return PyFloat_FromDouble(0.0L);
+#endif
 }
 
 PyObject*
 getAccelerationX(PyObject* self)
 {
+#ifdef CAVI_TEST_BUILD
   return PyFloat_FromDouble(99.99L);
+#else
+  // TODO: Add something to read from sensor data
+  return PyFloat_FromDouble(0.0L);
+#endif
 }
 
 PyObject*
 getAccelerationY(PyObject* self)
 {
+#ifdef CAVI_TEST_BUILD
   return PyFloat_FromDouble(99.99L);
+#else
+  // TODO: Add something to read from sensor data
+  return PyFloat_FromDouble(0.0L);
+#endif
 }
 
 PyObject*
 getAccelerationZ(PyObject* self)
 {
+#ifdef CAVI_TEST_BUILD
   return PyFloat_FromDouble(99.99L);
+#else
+  // TODO: Add something to read from sensor data
+  return PyFloat_FromDouble(0.0L);
+#endif
 }
 
 } // cavimodule::data
