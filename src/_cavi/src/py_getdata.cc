09475ea7b8d5152777debc52913ebd2115f883d0
MattToast <matthew.drozt@gmail.com>
2022-07-05 20:13:53 -0500

    add checks for mock hw on status getters/setters

--- src/_cavi/src/py_getdata.cc
+++ src/_cavi/src/py_getdata.cc
@@ -8,111 +8,101 @@ namespace cavimodule::data {
 PyObject*
 getAltitude(PyObject* self)
 {
-#ifdef CAVI_TEST_BUILD
-  return PyFloat_FromDouble(99.99L);
-#else
+  if (useMockHardware()) {
+    return PyFloat_FromDouble(99.99L);
+  }
   // TODO: Add something to read from sensor data
   return PyFloat_FromDouble(0.0L);
-#endif
 }
 
 PyObject*
 getLatitude(PyObject* self)
 {
-#ifdef CAVI_TEST_BUILD
-  return PyFloat_FromDouble(99.99L);
-#else
+  if (useMockHardware()) {
+    return PyFloat_FromDouble(99.99L);
+  }
   // TODO: Add something to read from sensor data
   return PyFloat_FromDouble(0.0L);
-#endif
 }
 
 PyObject*
 getLongitude(PyObject* self)
 {
-#ifdef CAVI_TEST_BUILD
-  return PyFloat_FromDouble(99.99L);
-#else
+  if (useMockHardware()) {
+    return PyFloat_FromDouble(99.99L);
+  }
   // TODO: Add something to read from sensor data
   return PyFloat_FromDouble(0.0L);
-#endif
 }
 
 PyObject*
 getTemperature(PyObject* self)
 {
-#ifdef CAVI_TEST_BUILD
-  return PyFloat_FromDouble(99.99L);
-#else
+  if (useMockHardware()) {
+    return PyFloat_FromDouble(99.99L);
+  }
   // TODO: Add something to read from sensor data
   return PyFloat_FromDouble(0.0L);
-#endif
 }
 
 PyObject*
 getGyroX(PyObject* self)
 {
-#ifdef CAVI_TEST_BUILD
-  return PyFloat_FromDouble(99.99L);
-#else
+  if (useMockHardware()) {
+    return PyFloat_FromDouble(99.99L);
+  }
   // TODO: Add something to read from sensor data
   return PyFloat_FromDouble(0.0L);
-#endif
 }
 
 PyObject*
 getGyroY(PyObject* self)
 {
-#ifdef CAVI_TEST_BUILD
-  return PyFloat_FromDouble(99.99L);
-#else
+  if (useMockHardware()) {
+    return PyFloat_FromDouble(99.99L);
+  }
   // TODO: Add something to read from sensor data
   return PyFloat_FromDouble(0.0L);
-#endif
 }
 
 PyObject*
 getGyroZ(PyObject* self)
 {
-#ifdef CAVI_TEST_BUILD
-  return PyFloat_FromDouble(99.99L);
-#else
+  if (useMockHardware()) {
+    return PyFloat_FromDouble(99.99L);
+  }
   // TODO: Add something to read from sensor data
   return PyFloat_FromDouble(0.0L);
-#endif
 }
 
 PyObject*
 getAccelerationX(PyObject* self)
 {
-#ifdef CAVI_TEST_BUILD
-  return PyFloat_FromDouble(99.99L);
-#else
+  if (useMockHardware()) {
+    return PyFloat_FromDouble(99.99L);
+  }
   // TODO: Add something to read from sensor data
   return PyFloat_FromDouble(0.0L);
-#endif
 }
 
 PyObject*
 getAccelerationY(PyObject* self)
 {
-#ifdef CAVI_TEST_BUILD
-  return PyFloat_FromDouble(99.99L);
-#else
+  if (useMockHardware()) {
+    return PyFloat_FromDouble(99.99L);
+  }
   // TODO: Add something to read from sensor data
   return PyFloat_FromDouble(0.0L);
-#endif
 }
 
 PyObject*
 getAccelerationZ(PyObject* self)
 {
-#ifdef CAVI_TEST_BUILD
-  return PyFloat_FromDouble(99.99L);
-#else
+  if (useMockHardware()) {
+    return PyFloat_FromDouble(99.99L);
+  }
   // TODO: Add something to read from sensor data
   return PyFloat_FromDouble(0.0L);
-#endif
 }
 
 } // cavimodule::data
