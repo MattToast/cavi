89fa4a596e4e84ef27b85f2ec69832bdcb064413
MattToast <matthew.drozt@gmail.com>
2022-07-06 02:31:48 -0500

    add option to run cavi with socket

--- src/cavi/__main__.py
+++ src/cavi/__main__.py
@@ -1,10 +1,18 @@
-from orbitalcoms import create_serial_launch_station
+from orbitalcoms import create_serial_launch_station, create_socket_launch_station
+import sys
 
 import cavi._app as app
 
 
 def main() -> int:
-    app.run_app(create_serial_launch_station(port="/dev/ttyUSB0", baudrate=9600))
+    if len(sys.argv) > 1 and sys.argv[1].lower() == "socket":
+        ls = create_socket_launch_station(host="127.0.1.1", port=5000)
+    else:
+        ls = create_serial_launch_station(port="/dev/ttyUSB0", baudrate=9600)
+
+    with ls:
+        app.run_app(ls)
+
     return 0
 
 
